0ae101532094312281280c47d02e75ed44f7507c
Bruno Alves <brunoades@gmail.com>
2022-07-28 20:16:07 -0300

    Issue 9 (#13)
    * turned into installable binary #9
    
    * solved conflicts #9

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+from importlib.metadata import entry_points
 from setuptools import setup, find_packages
 
 setup(
@@ -6,4 +7,9 @@ setup(
     description="Reward Point System for Dunder Mifflin",
     author="Bruno A de Souza",
     packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "dundie = dundie.__main__:main"
+        ]
+    }
 )
