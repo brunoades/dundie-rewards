edeac7822976eb4f4e55e6a029e0e22c7d2c8e4f
Bruno Alves <brunoades@gmail.com>
2022-07-28 20:54:18 -0300

    start of the CLI interface

--- dundie/__main__.py
+++ dundie/__main__.py
@@ -1,2 +1,36 @@
+import argparse
+
+
+def load(filepath):
+    """Loads data from file to database"""
+    try:
+        with open(filepath) as file_:
+            for line in file_:
+                print(line)
+    except FileNotFoundError as e:
+        print(f"File not found {e}")
+
 def main():
-    print("Executing dundie from entry point")
+
+    parser = argparse.ArgumentParser(
+        description="Dunder Mifflin Rewards CLI",
+        epilog="Enjoy and use with caution!",
+    )
+    parser.add_argument(
+        "subcommand",
+        type=str,
+        help="the subcommand to run",
+        choices=("load", "show", "send"),
+        default="help"
+    )
+    parser.add_argument(
+        "filepath",
+        type=str,
+        help="file path to load from",
+    )
+    args = parser.parse_args()
+    globals()[args.subcommand](args.filepath)
+
+
+if __name__ == "__main__":
+    main()
